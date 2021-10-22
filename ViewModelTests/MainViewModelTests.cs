ebcbb544e781aa29e94a61e31ac789eb1bcf5cca
Festino <offestashka@mail.ru>
2021-10-22 21:28:12 +0300

    Task2 first commit

---
+++ ViewModelTests/MainViewModelTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ViewModelTests
+{
+    [TestClass]
+    public class MainViewModelTests
+    {
+        [TestMethod]
+        public void A()
+        {
+            Assert.AreEqual(0, 0, 1e-4);
+        }
+    }
+}
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using _.Utils;
+
+namespace Tests
+{
+    [TestClass]
+    public class UtilsTests
+    {
+        [TestMethod]
+        public void NormalizeNullArg()
+        {
+            Assert.AreEqual(0, 0, 1e-4);
+        }
+
+        [TestMethod]
+        public void NormalizeEmptyArr()
+        {
+            Assert.AreEqual(0, 0, 1e-4);
+        }
+
+        [TestMethod]
+        public void NormalizeZero()
+        {
+            Assert.AreEqual(0, 0, 1e-4);
+        }
+
+        [TestMethod]
+        public void NormalizeValidVector()
+        {
+            double[] arg = { 8, 6, 0 };
+            double[] res = Utils.Normalize(arg);
+            Assert.AreEqual(0.8, res[0], 1e-6);
+            Assert.AreEqual(0.6, res[1], 1e-6);
+            Assert.AreEqual(0, res[2], 1e-6);
+        }
+    }
+}
---
+++ ViewModelTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ViewModelTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ViewModelTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("234cb980-daff-4b4c-8403-13be0cb045d6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
